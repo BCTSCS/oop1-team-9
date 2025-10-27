ca795e8ef01d24d3377f38d6678000fe0fefc458
Addysaurus <addisonalmagor@gmail.com>
2025-10-27 14:03:49 -0400

    Player and Vlogger class

---
+++ src/main/java/Vlogger.java
@@ -0,0 +1,17 @@
+public class Vlogger {
+    private String name;
+    private int yearJoined;
+
+    public Vlogger() {
+        name = "Addy";
+        yearJoined = 2025;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearJoined() {
+        return yearJoined;
+    }
+}
